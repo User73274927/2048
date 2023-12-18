fbcc802136fe8d777421ff3b6a76c9a71a7c15c0
dzunko <kodzun567@gmail.com>
2023-12-18 17:18:04 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -2,15 +2,31 @@ const canvas = document.getElementById('game-screen');
 const ctx = canvas.getContext('2d');
 
 const score = document.getElementById('score')
+const max_score = document.getElementById('max-score');
+const new_game = document.getElementById('new-game-button');
 
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
-let isLose = false;
 const board = new Board(canvas.width, 0, 0);
 const game = new Game();
+let isLose = false;
+
 game.init();
 board.setCells(game.getCells());
+
+function load() {
+    let saved_max_score = sessionStorage.getItem("max-score");
+    max_score.innerHTML = (saved_max_score !== null) ? saved_max_score : '0';
+}
+
+function restartGame() {
+    if (isLose) return true;
+    return confirm(
+        "Вы уверены, что хотите перезапустить игру?\n" +
+        "Текущая игра и счет будут утеряны!!"
+    );
+}
 
 const clear = () => {
     ctx.fillStyle = "#ffffff";
@@ -24,6 +40,18 @@ const gameLoop = () => {
 
 let draw = () => {
     board.draw(ctx);
+}
+
+function update() {
+    game.turn();
+    board.setCells(game.getCells());
+    game.mergeCells();
+    board.setCells(game.getCells());
+    score.innerHTML = game.score.toString();
+    maxScoreChanged();
+    if (game.checkLose()) {
+        isLose = true;
+    }
 }
 
 function keyDown(e) {
@@ -49,25 +77,29 @@ function keyDown(e) {
             game.setDirection(Direction.RIGHT);
             break;
     }
-    game.turn();
-    board.setCells(game.getCells());
-    game.mergeCells();
-    board.setCells(game.getCells());
-    score.innerHTML = game.score.toString();
-    if (game.checkLose()) {
-        isLose = true;
+    update();
+}
+
+function maxScoreChanged() {
+    if (Number(score.innerHTML) > Number(max_score.innerHTML)) {
+        max_score.innerHTML = score.innerHTML;
     }
 }
 
-function transition(option) {
-
-}
-
 window.addEventListener("keydown", keyDown);
+new_game.addEventListener("click", (e) => {
+    if (restartGame()) {
+        window.location.reload();
+    }
+});
 
+
+
+load();
 const loop = setInterval(() => {
     gameLoop();
     if (isLose) {
+        sessionStorage.setItem("max-score", max_score.innerHTML);
         alert("Ты проиграл((\nТвой результат:   " + game.score.toString());
         clearInterval(loop);
     }
